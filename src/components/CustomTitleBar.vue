579344d3e287e007cd62a3d10ed44d3a3576172d
miket.io <mdt_@msn.com>
2025-01-05 17:03:30 -0500

    Fixed build error.

--- src/components/CustomTitleBar.vue
+++ src/components/CustomTitleBar.vue
@@ -4,21 +4,25 @@
       <img src="/logo.svg" alt="App Logo" />
     </div>
     <div class="titlebar-controls titlebar-controls-macos" v-if="isMacOS">
-      <button class="titlebar-button close" @click="window.ipc.send('window-control', 'close')"></button>
-      <button class="titlebar-button minimize" @click="window.ipc.send('window-control', 'minimize')"></button>
-      <button class="titlebar-button maximize" @click="window.ipc.send('window-control', 'maximize')"></button>
+      <button class="titlebar-button close" @click="handleWindowControl('close')"></button>
+      <button class="titlebar-button minimize" @click="handleWindowControl('minimize')"></button>
+      <button class="titlebar-button maximize" @click="handleWindowControl('maximize')"></button>
     </div>
     <div class="titlebar-drag-region">
       <span class="titlebar-title">N2S Elements</span>
     </div>
     <div class="titlebar-controls titlebar-controls-windows" v-if="!isMacOS">
-      <button class="titlebar-button minimize" @click="window.ipc.send('window-control', 'minimize')"></button>
-      <button class="titlebar-button maximize" @click="window.ipc.send('window-control', 'maximize')"></button>
-      <button class="titlebar-button close" @click="window.ipc.send('window-control', 'close')"></button>
+      <button class="titlebar-button minimize" @click="handleWindowControl('minimize')"></button>
+      <button class="titlebar-button maximize" @click="handleWindowControl('maximize')"></button>
+      <button class="titlebar-button close" @click="handleWindowControl('close')"></button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const isMacOS = window.os.platform === 'darwin';
+
+const handleWindowControl = (action: 'close' | 'minimize' | 'maximize') => {
+  window.ipc.send('window-control', action);
+};
 </script>
